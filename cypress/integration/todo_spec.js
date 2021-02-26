7d11281786e2350ff5c66c30aefb14dd3b4895a5
Olena Onishchenko <o.onishchenko@corgihomeplan.co.uk>
2021-02-26 12:12:36 +0200

    Added 2 more tests

--- cypress/integration/todo_spec.js
+++ cypress/integration/todo_spec.js
@@ -2,7 +2,8 @@ describe('TodoMVC - React', () => {
   // items description
   const NEW_ITEMS = [
     'My first todo',
-    'My second todo'
+    'My second todo',
+    'My third todo'
   ]
   const EDITED_ITEM = 'Updated todo'
 
@@ -63,5 +64,34 @@ describe('TodoMVC - React', () => {
 
     todoItems()
       .should('contain', EDITED_ITEM)
+  })
+
+  it('should clear completed items', () => {
+    cy.get('.new-todo')
+    .type(NEW_ITEMS[0]).type('{enter}')
+    .type(NEW_ITEMS[1]).type('{enter}')
+     cy.get('.toggle[type="checkbox"]')
+    .first()
+    .check()
+    .should('be.checked')
+    cy.contains('Completed')
+    .click()
+    todoItems().should('have.length', 1)
+    cy.get('.clear-completed')
+    .click()
+    todoItems().should('have.length', 0)
+  })
+  
+  it.only('should properly count an items when delete', () => {
+    cy.get('.new-todo')
+    .type(NEW_ITEMS[0]).type('{enter}')
+    .type(NEW_ITEMS[1]).type('{enter}')
+    .type(NEW_ITEMS[2]).type('{enter}')
+    cy.get('.todo-count')
+    .should('have.text', '3 items left')
+    cy.get('.destroy').eq("2").click({force: true})
+    cy.get('.todo-count')
+    .should('have.text', '2 items left')
+
   })
 })
